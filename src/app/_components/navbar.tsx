2e47048d9b727742f0d7a704377131fa9d2f3cac
YiJun Zou <zapboltisepic@gmail.com>
2025-02-17 19:28:24 +1100

    fixed prod issues

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -15,7 +15,7 @@ export default async function NavBar() {
                         <button aria-label="Open base settings menu" className="flex place-items-center flex-auto max-w-480">
                             <div className="flex place-items-center huge pointer line-height-3 focus-visible-current-color rounded css-w1u7fo lightColoredBase min-w-0 flex-initial">
                                 <div className="truncate font-family-display-updated heading-size-small strongest min-w-0 flex-initial leading-6">
-                                    YiJun's Copy of Airtable
+                                    YiJun Copy of Airtable
                                 </div>
                                 <svg className="w-4 h-4 flex-none ml-half" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <polyline points="6 9 12 15 18 9"></polyline>
