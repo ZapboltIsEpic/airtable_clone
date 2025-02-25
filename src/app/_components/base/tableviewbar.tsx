40b8e7d975342e573dfd30dfd10ed464da9e5d17
YiJun Zou <zapboltisepic@gmail.com>
2025-02-25 14:24:02 +1100

    ui fixed for tableviewbar

--- src/app/_components/base/tableviewbar.tsx
+++ src/app/_components/base/tableviewbar.tsx
@@ -4,24 +4,29 @@ import Image from "next/image";
 
 export default function TableViewBar() {
     return (
-        <div className="flex flex-none h-11">
+        <div className="flex flex-none h-11 z-10">
             <div className="flex-none flex items-center">
                 <div className="h-full flex items-center pl-3">
-                    <button className="relative rounded-full flex items-center pointer mr-2 h-7">
+                    <button className="relative flex items-center pointer mr-2 h-7 px-3 hover:bg-[rgb(242,242,242)]">
                         <span className="flex items-center flex-none">
                             <Image src="hamburger-menu-svgrepo-com.svg" alt="." width={16} height={16} />
                         </span>
-                        <span className="flex-none strong ml-half">Views</span>
+                        <span className="flex-none strong ml-1 text-[13px]">Views</span>
                     </button>
-                    <button className="flex items-center pointer px-2 h-7">
+                    <div className="flex justify-center items-center ml-1 mr-3">
+                        <div className="border-l border-gray-600 border-opacity-20 h-4"></div>
+                    </div>
+                    <button className="flex items-center pointer px-2 h-7 hover:bg-[rgb(242,242,242)]">
                         <div className="flex items-center">
                             <Image src="/tables.PNG" alt="." width={16} height={16} />
-                            <h2>Grid view</h2>
+                            <h2 className="text-[13px]">Grid view</h2>
                             <div className="flex mx-2">
                                 <Image src="/users3-svgrepo-com.svg" alt="." width={16} height={16} />
                             </div>
                             <div>
-                                <Image src="down-chevron-svgrepo-com.svg.svg" alt="." width={16} height={16} />
+                                <svg className="w-4 h-4 flex-none ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                    <polyline points="6 9 12 15 18 9"></polyline>
+                                </svg>
                             </div>
                         </div>
                     </button>
@@ -33,46 +38,46 @@ export default function TableViewBar() {
                         <div className="flex items-center">
                             <div className="flex flex-row mr-2">
                                 <div>
-                                    <button className="mr-2 focus-visible">
+                                    <button className="mr-2 focus-visible hover:bg-[rgb(242,242,242)]">
                                         <div className="pointer flex items-center px-2 rounded-full py-1">
                                             <Image src="eye-slash-visibility-visible-hide-hidden-show-watch-svgrepo-com.svg" alt="." width={16} height={16} />
-                                            <div className="ml-1">Hide fields</div>
+                                            <div className="ml-1 text-[13px]">Hide fields</div>
                                         </div>
                                     </button>
                                 </div>
-                                <button>
+                                <button className="hover:bg-[rgb(242,242,242)]">
                                     <div className="pointer flex items-center px-2 rounded-full py-1">
                                         <Image src="funnel-simple-thin-svgrepo-com.svg" alt="." width={16} height={16} />
-                                        <div className="ml-1">Filter</div>
+                                        <div className="ml-1 text-[13px]">Filter</div>
                                     </div>
                                 </button>
                             </div>
                             <div className="flex items-center">
                                 <div>
-                                    <button className="mr-2 focus-visible">
+                                    <button className="mr-2 focus-visible hover:bg-[rgb(242,242,242)]">
                                         <div className="pointer flex items-center px-2 rounded-full py-1">
                                             <Image src="/group.PNG" alt="." width={16} height={16} />
-                                            <div className="ml-1">Group</div>
+                                            <div className="ml-1 text-[13px]">Group</div>
                                         </div>
                                     </button>
                                 </div>
                                 <div>
-                                    <button className="mr-2 focus-visible">
+                                    <button className="mr-2 focus-visible hover:bg-[rgb(242,242,242)]">
                                         <div className="pointer flex items-center px-2 rounded-full py-1">
                                             <Image src="arrows-down-up-duotone-svgrepo-com.svg" alt="." width={16} height={16} />
-                                            <div className="ml-1">Sort</div>
+                                            <div className="ml-1 text-[13px]">Sort</div>
                                         </div>
                                     </button>
                                 </div>
                             </div>
-                            <button className="mr-2">
+                            <button className="mr-2 hover:bg-[rgb(242,242,242)]">
                                 <div className="pointer flex items-center rounded px-2 py-1">
                                     <Image src="paint-bucket-thin-svgrepo-com.svg" alt="." width={16} height={16} />
-                                    <div className="ml-1">Color</div>
+                                    <div className="ml-1 text-[13px]">Color</div>
                                 </div>
                             </button>
                             <div>
-                                <button>
+                                <button className="hover:bg-[rgb(242,242,242)]">
                                     <div className="pointer flex items-center px-2 rounded-full py-1">
                                         <Image src="/row-height-small.PNG" alt="." width={16} height={16} />
                                     </div>
@@ -80,19 +85,19 @@ export default function TableViewBar() {
                             </div>
                         </div>
                         <span className="flex items-center">
-                            <button className="flex items-center px-1 rounded pointer mr-2">
+                            <button className="flex items-center px-1 rounded pointer mr-2 hover:bg-[rgb(242,242,242)]">
                                 <div className="flex">
                                     <div className="flex items-center p-1">
                                         <Image src="/arrows-square-out.PNG" alt="." width={16} height={16} />
                                     </div>
-                                    <div className="py-1 pr-1">Share and sync</div>
+                                    <div className="text-[13px] py-1 pr-1">Share and sync</div>
                                 </div>
                             </button>
                         </span>
                     </div>
                 </div>
             </div>
-            <button className="flex flex-none">
+            <button className="flex flex-none w-8">
                 <div className="flex items-center">
                     <Image src="/magnifying-glass-backup-svgrepo-com.svg" alt="." width={16} height={16} />
                 </div>
