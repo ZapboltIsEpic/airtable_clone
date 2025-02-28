e82a39fee7509823b08d412b6161cfeea4210be3
YiJun Zou <zapboltisepic@gmail.com>
2025-02-28 15:28:22 +1100

    fixing deployment issues

--- src/app/_components/home/homesidebar.tsx
+++ src/app/_components/home/homesidebar.tsx
@@ -22,7 +22,7 @@ export default function HomeSideBar({ session, isExpanded }: { session: Session;
 
     const expanded = isHovered || isExpanded;
 
-    const { mutate: createBase, isLoading, error } = api.base.create.useMutation({
+    const { mutate: createBase, error } = api.base.create.useMutation({
         // instead take him straight to the base
         onSuccess: () => {
           console.log("Base created successfully");
