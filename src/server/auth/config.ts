d49bae26a0978f7faa4f56b73e36facba651a0ef
YiJun Zou <zapboltisepic@gmail.com>
2025-02-28 17:13:30 +1100

    fixing deployment issues

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,72 +1,72 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+// import { type DefaultSession, type NextAuthConfig } from "next-auth";
+// import DiscordProvider from "next-auth/providers/discord";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-import { db } from "~/server/db";
+// import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+// /**
+//  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+//  * object and keep type safety.
+//  *
+//  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+//  */
+// declare module "next-auth" {
+//   interface Session extends DefaultSession {
+//     user: {
+//       id: string;
+//       // ...other properties
+//       // role: UserRole;
+//     } & DefaultSession["user"];
+//   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
+//   // interface User {
+//   //   // ...other properties
+//   //   // role: UserRole;
+//   // }
+// }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
-export const authConfig = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    DiscordProvider,
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "user@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: Partial<Record<"email" | "password", unknown>>) {
-        const { email, password } = credentials as { email?: string; password: string };
-        if (!email || !password) return null;
+// /**
+//  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+//  *
+//  * @see https://next-auth.js.org/configuration/options
+//  */
+// export const authConfig = {
+//   adapter: PrismaAdapter(db),
+//   providers: [
+//     DiscordProvider,
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "Email", type: "email", placeholder: "user@example.com" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials: Partial<Record<"email" | "password", unknown>>) {
+//         const { email, password } = credentials as { email?: string; password: string };
+//         if (!email || !password) return null;
 
-        const user = await db.user.findUnique({
-          where: { email: credentials.email as string },
-        });
+//         const user = await db.user.findUnique({
+//           where: { email: credentials.email as string },
+//         });
 
-        if (!user) throw new Error("No user found");
+//         if (!user) throw new Error("No user found");
 
-        return user;
-      },
-    }),
-  ],
-  callbacks: {
-    // session: ({ session, user }) => ({
-    //   ...session,
-    //   user: {
-    //     ...session.user,
-    //     id: user.id,
-    //   },
-    // }),
-  },
-  session: {
-    strategy: "database", // This is optional but helps make it clear that you want to store sessions in the database.
-  },
-}
+//         return user;
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     session: ({ session, user }) => ({
+//       ...session,
+//       user: {
+//         ...session.user,
+//         id: user.id,
+//       },
+//     }),
+//   },
+//   session: {
+//     strategy: "database", // This is optional but helps make it clear that you want to store sessions in the database.
+//   },
+// }
