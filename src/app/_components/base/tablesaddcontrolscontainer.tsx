f28ce418f762c2e9bdec56ab2e1bddda2140273e
YiJun Zou <zapboltisepic@gmail.com>
2025-03-01 15:29:07 +1100

    fixing deployment issues

--- src/app/_components/base/tablesaddcontrolscontainer.tsx
+++ src/app/_components/base/tablesaddcontrolscontainer.tsx
@@ -39,10 +39,10 @@ export default function TablesAddControlsContainer({base} : { base : Base}) {
             const newTableData = {
                 name: newTable.name,
                 id: `temp-${tablesData.length + 1}`,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                baseId: newTable.baseid, 
-                numberedId: tablesData.length + 1,
+                createdat: new Date(),
+                updatedat: new Date(),
+                baseid: newTable.baseid, 
+                numberedid: tablesData.length + 1,
             }
             const newTablesData = [...tablesData, newTableData];
             setTablesData(newTablesData); 
@@ -52,8 +52,8 @@ export default function TablesAddControlsContainer({base} : { base : Base}) {
         
         onError: (error, context) => {
             console.error('Error creating table:', error);
-            if (context?.previousTablesData) {
-                setTablesData(context.previousTablesData);
+            if (context) {
+                setTablesData(context as unknown as Table[]);
             }  
         
             // queryClient.setQueryData('tableData', context.previousTableData);
