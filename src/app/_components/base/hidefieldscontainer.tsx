7c657e3dccb43210a30828c4093894f5700c7c13
YiJun Zou <zapboltisepic@gmail.com>
2025-03-06 11:32:33 +1100

    non-transparent hide fields now.

--- src/app/_components/base/hidefieldscontainer.tsx
+++ src/app/_components/base/hidefieldscontainer.tsx
@@ -31,7 +31,7 @@ export default function HideFieldsContainer({ tableid }: { tableid: string }) {
     };
     
     return (
-        <div className="absolute top-[123px] left-[259px] z-50 min-w-[105px]">
+        <div className="absolute top-[123px] left-[259px] z-50 min-w-[105px] opacity-100 shadow-lg backdrop-blur-none bg-white">
             <div className="mt-2 mx-4 flex items-center border-b-2 border-solid">
                 <input type="text" placeholder="Find a field" className="flex-auto small px-0 py-2" value="" />
                 <button className="font-light mx-2 flex place-items-center justify-items-center h-7 px-3 rounded-full">
